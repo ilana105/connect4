0c3122f7023d872e99aea0dbeaea1d3d1a74c6d7
Ilana Leva <35470918+ilana105@users.noreply.github.com>
2018-04-04 16:17:42 -0400

    updated the board positioning and who can go

--- connect4.js
+++ connect4.js
@@ -1,44 +1,173 @@
-let gameField = new Array();
-let board = document.getElementById("game-table");
-let currCol;
-let currRow;
 let currPlayer;
+
 const player1 = 1;
 const player2 = 2;
+let player1startColor = "";
+let player2startColor = "";
 
-newgame();
+const numCols = 7;
+const numRows = 6;
+const cellWidth = 85;
+const cellSpacing = 1;
 
-/*function that creates the 6x7 board
-2D array, each index with a number in it representing the 'value' of that position
-0 - empty
-1 - player1
-2 - player2
+
+const container = document.querySelector("#gridContainer");
+let chip = document.querySelectorAll('span.chip');
+let p1changedColor = document.querySelector("div.player1");
+let p2changedColor = document.querySelector("div.player2");
+let p1Button = document.querySelector("#p1ColorChange");
+let p2Button = document.querySelector("#p2ColorChange");
+
+//----------------Create board--------------------------
+let screenSize;
+
+if (screen.width > 1500)
+	screenSize = screen.width * .34;
+
+else 
+	screenSize = screen.width * .26;
+
+container.style.left = screenSize + "px";
+
+const span = document.createElement('span');
+span.className = 'cell';
+
+const cells = [];
+
+for (let row = 0; row < numRows; row++) {
+	cells.push([]);
+	for (let col = 0; col < numCols; col++) {
+		let cell = span.cloneNode();
+		cell.style.left = `${col * (cellWidth + cellSpacing)}px`;
+		cell.style.top = `${row * (cellWidth + cellSpacing)}px`;
+		container.appendChild(cell);
+		cells[row][col] = cell;
+	}
+}
+//--------------------End Board-------------------------
+function startGame() {
+	//Decides who goes first by random
+	let randNum = (Math.floor(Math.random() * 2) + 1);
+
+	if (randNum == 1) {
+		currPlayer = 1;
+		chip[0].style.visibility = "visible";
+		chip[1].style.visibility = "hidden";
+	}
+
+	else {
+		currPlayer = 2;
+		chip[0].style.visibility = "hidden";
+		chip[1].style.visibility = "visible";
+	}
+
+	p1Button.style.visibility = "hidden"; //hides the buttons to change colors, so that the players cant change colors during the game
+	p2Button.style.visibility = "hidden";
+	document.getElementById("restartGame").style.visibility = "visible";
+	document.getElementById("startGame").style.visibility = "hidden";
+
+	player1startColor = p1changedColor.style.color;
+	player2startColor = p2changedColor.style.color;
+}
+
+function restartGame() {
+	p1Button.style.visibility = "visible";
+	p2Button.style.visibility = "visible";
+  	document.getElementById("restartGame").style.visibility = "hidden";
+	document.getElementById("startGame").style.visibility = "visible";
+
+	chip[0].style.visibility = "hidden";
+	chip[1].style.visibility = "hidden";
+
+	player1startColor = "";
+	player2startColor = "";
+
+	let cells = document.getElementsByClassName("cellSelected");
+	//debugger;
+	for (let el of cells){
+		el.style.backgroundColor = "#FFF";
+		el.className = "cell";
+	}
+}
+
+//-----------------------------COLOR Options------------------------------
+//default color values for the player
+let colorPalette1 = ["#ff9999", "#CC0000", "#800000"];
+let colorPalette2 = ["#99e6ff", "#0099cc", "#006080"];
+
+//returns the color of the disc when the user clicks a button
+let index = 0;
+function changeColor(array) {
+ 	index++;
+
+ 	if (index == 3)
+ 		index = 0;
+
+	return array[index]
+}
+
+//changes the chip color depending on which button was pressed
+function changeChipColor (p) {
+	if (p == 1)
+		chip[0].style.backgroundColor = p1changedColor.style.color;
+
+	else if (p == 2)
+		chip[1].style.backgroundColor = p2changedColor.style.color;
+}
+//-------------------------------------------------------------------------
+
+//add events to the buttons when clicked
+p1Button.addEventListener("click", () => {changeChipColor(1); }, false);
+p2Button.addEventListener("click", () => {changeChipColor(2); }, false);
+
+
+container.onclick = placeChip;
+
+
+function placeChip (e) {
+	let rect = container.getBoundingClientRect();
+	let mouseX = e.clientX - rect.left;
+	let mouseY = e.clientY - rect.top;
+	let columnWidth = cellWidth + cellSpacing;
+	let col = Math.floor(mouseX/columnWidth);
+	let row = Math.floor(mouseY/columnWidth);
+	let newCol = check(row, col)
+	let selectedCell = cells[row][newCol];
+
+	if (isOccupied(row, newCol)) {
+		//alert("spot is taken");
+	}
+	selectedCell.className = 'cellSelected';
+	console.log(`${col},${row}`);
+
+	if (currPlayer == 1) {
+		selectedCell.style.backgroundColor = player1startColor;
+		currPlayer = 2;
+		chip[0].style.visibility = "hidden";
+		chip[1].style.visibility = "visible";
+	}
+
+	else {
+		selectedCell.style.backgroundColor = player2startColor;
+		currPlayer = 1;
+		chip[0].style.visibility = "visible";
+		chip[1].style.visibility = "hidden";
+	}
+}
+
+/*
+Checks to see if the column has other pieces
 */
-function prepareField(){
-  gameField = new Array();
-  for(let row = 0; row < 6; row++){
-    gameField[row] = new Array();
-    for(let col = 0; col < 7; col++){
-      gameField[row].push(0);
-    }
-  }
+function check(row, col) {
+	for (let i = 6; i < col; i--) {
+		let nextCell = cells[row][i];
+		if (nextCell.style.backgroundColor == "#FFF")
+			return i;
+	}
+	return col;
 }
 
-/*initialize the board and starts a new game.
-  randNum = randommize who goes first
-*/
-function newGame(){
-  prepareField();
-  let randNum = (Math.floor(Math.random() * 2) + 1);
-  placeDisc(randNum);
-}
-
-//function that places the disc in its respective position but doesn't display on screen
-function placeDisc(row,col){
-  // TODO:
-}
-
-//function that creates the disc
-function makeDisc(player){
-  // TODO:
+function isOccupied(row, col) {
+	let thisCell = cells[row][col];
+	return thisCell === 0 ? false : true;
 }
